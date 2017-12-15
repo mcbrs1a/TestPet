855e4523fc6863b0f527f2de562ed2e01e5d382a
Rhodri Smith <mcbrs1a@gmail.com>
2017-12-15 23:49:02 +0000

    third push

--- gym_PET/envs/PET_env.py
+++ gym_PET/envs/PET_env.py
@@ -6,11 +6,16 @@ from gym.utils import seeding
 class PETEnv(gym.Env):
   metadata = {'render.modes': ['human', 'rgb_array']}
 
-  def __init__(self):
-    ...
-  def _step(self, action):
-    ...
-  def _reset(self):
-    ...
-  def _render(self, mode='human', close=False):
-    ...
+def __init__(self):
+	self.__version__ = "0.1.0"
+	print("PET_env - Version {}".format(self.__version__))
+
+# General variables defining the environment
+
+   
+def _step(self, action):
+    
+def _reset(self):
+    
+def _render(self, mode='human', close=False):
+    
---
+++ gym_PET/envs/PET_read.py
@@ -0,0 +1,29 @@
+#PET_read.py
+#Read a PET image #2D for now and output a numpy array
+#pip install nibabel
+#http://nipy.org/nibabel/coordinate_systems.html#introducing-someone
+
+
+import numpy as np
+import nibabel as nib
+import matplotlib.pyplot as plt
+
+def get_data
+	PET_img = nib.load('TestPet/gym_PET/envs/io/test.nii')
+	PET_img_data = PET_img.get_data()
+	PET_img_data.shape
+	retrun PET_img_data
+
+def show_slices(slices):
+	""" Function to display row of image slices """
+	fig, axes = plt.subplots(1, len(slices))
+	for i, slice in enumerate(slices):
+		axes[i].imshow(slice.T, cmap="gray", origin="lower")
+
+slice_0 = PET_img_data[125, :, :,0]
+slice_1 = PET_img_data[:, 160, :,0]
+slice_2 = PET_img_data[:, :, 100,0]
+
+show_slices([slice_0, slice_1, slice_2])
+plt.suptitle("Center slices for EPI image")
+
